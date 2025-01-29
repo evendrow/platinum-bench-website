7f0df3ea5e773ffa09bb17b4fe2feadd9971e579
Edward Vendrow <evendrow@gmail.com>
2025-01-29 13:14:30 -0500

    Google analytics script

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-GYK0PMY2N4" />
     </html>
   );
 }
